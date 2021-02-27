958fce95b928f5de69f3537c2d7ce92da60260ec
Veeresh <57994727+Veeresh603@users.noreply.github.com>
2021-02-28 00:23:21 +0530

    Commit made using teleportHQ

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ const Page = () => {
       <h2 className={` ${projectStyles.thqHeading2} ${styles.text} `}>Your data has a story</h2>
       <img
         src="/playground_assets/image%202-1100h.png"
-        className={` ${projectStyles.thqImage} ${styles.image1} `}
+        className={` ${projectStyles.thqImage} ${styles.image01} `}
       />
       <h2 className={` ${projectStyles.thqHeading2} ${styles.text01} `}>
         Some of your Data-driven services
@@ -30,7 +30,7 @@ const Page = () => {
         <div className={styles.container03}>
           <img
             src="/playground_assets/toggle.svg"
-            className={` ${projectStyles.thqImage} ${styles.image2} `}
+            className={` ${projectStyles.thqImage} ${styles.image02} `}
           />
           <div className={styles.container04}>
             <h2 className={` ${projectStyles.thqHeading2} ${styles.text02} `}>
@@ -49,7 +49,7 @@ const Page = () => {
         <div className={styles.container06}>
           <img
             src="/playground_assets/robot.svg"
-            className={` ${projectStyles.thqImage} ${styles.image3} `}
+            className={` ${projectStyles.thqImage} ${styles.image03} `}
           />
           <div className={styles.container07}>
             <h2 className={` ${projectStyles.thqHeading2} ${styles.text04} `}>
@@ -68,7 +68,7 @@ const Page = () => {
         <div className={styles.container09}>
           <img
             src="/playground_assets/medal%201-1100h.png"
-            className={` ${projectStyles.thqImage} ${styles.image4} `}
+            className={` ${projectStyles.thqImage} ${styles.image04} `}
           />
           <div className={styles.container10}>
             <h2 className={` ${projectStyles.thqHeading2} ${styles.text06} `}>
@@ -93,7 +93,7 @@ const Page = () => {
         <div className={styles.container13}>
           <img
             src="/playground_assets/image%207-1100h.png"
-            className={` ${projectStyles.thqImage} ${styles.image5} `}
+            className={` ${projectStyles.thqImage} ${styles.image05} `}
           />
           <h3 className={` ${projectStyles.thqHeading3} ${styles.text09} `}>
             Diploma in Machine Learning &amp; AI
@@ -106,7 +106,7 @@ const Page = () => {
         <div className={styles.container14}>
           <img
             src="/playground_assets/image%208-1100h.png"
-            className={` ${projectStyles.thqImage} ${styles.image6} `}
+            className={` ${projectStyles.thqImage} ${styles.image06} `}
           />
           <h3 className={` ${projectStyles.thqHeading3} ${styles.text11} `}>
             Cloud Security Practitioner
@@ -120,7 +120,7 @@ const Page = () => {
         <div className={styles.container15}>
           <img
             src="/playground_assets/image%204-1100h.png"
-            className={` ${projectStyles.thqImage} ${styles.image7} `}
+            className={` ${projectStyles.thqImage} ${styles.image07} `}
           />
           <h3 className={` ${projectStyles.thqHeading3} ${styles.text13} `}>
             Diploma in Machine Learning &amp; AI
@@ -129,6 +129,50 @@ const Page = () => {
             Machine Learning is rapidly developing field. Over 50,000 jobs are vacant in India
           </p>
           <button className={` ${projectStyles.thqButton} ${styles.button2} `}>Read More</button>
+        </div>
+      </div>
+      <div className={styles.container16}>
+        <img
+          src="/playground_assets/image%209-1100h.png"
+          className={` ${projectStyles.thqImage} ${styles.image08} `}
+        />
+      </div>
+      <div className={styles.container17}>
+        <div className={styles.div}>
+          <img
+            src="/playground_assets/image%2010-1100h.png"
+            className={` ${projectStyles.thqImage} ${styles.image09} `}
+          />
+          <h2 className={` ${projectStyles.thqHeading2} ${styles.text15} `}>
+            Analogica offers an in-depth certification course on Data Science, Machine learning and
+            Artificial intelligence. This course is taught from scratch. The only pre-requisite is
+            that the student must have the right educational background to achieve success.
+          </h2>
+          <h3 className={` ${projectStyles.thqHeading3} ${styles.text16} `}>
+            This course is designed to make you a machine learning engineer. However, those who wish
+            to train for data analytics and business analytics role also will find this course
+            extremely useful. The course enrollment is free for the first 4 classes. Once the
+            student is satisfied that the teaching style , scope of syllabus ans the technology used
+            are adequate, student can officially sign-up for the course
+          </h3>
+          <button className={` ${projectStyles.thqButton} ${styles.button3} `}>Read More</button>
+        </div>
+        <div className={styles.div1}>
+          <img
+            src="/playground_assets/image%2011-1100h.png"
+            className={` ${projectStyles.thqImage} ${styles.image10} `}
+          />
+          <h2 className={` ${projectStyles.thqHeading2} ${styles.text17} `}>
+            Analogica offers an in-depth certification course on Data Science, Machine learning and
+            Artificial intelligence. This course is taught from scratch. The only pre-requisite is
+            that the student must have the right educational background to achieve success.
+          </h2>
+          <h3 className={` ${projectStyles.thqHeading3} ${styles.text18} `}>
+            The world of information and cybersecurity seems to be changing almost daily. The right
+            cybersecurity resources can help you stay informed. We have a page dedicated to Ethical
+            Hacking.
+          </h3>
+          <button className={` ${projectStyles.thqButton} ${styles.button4} `}>Read More</button>
         </div>
       </div>
     </div>
